693c77b980a9cc8ff2c0335e58a08c4788911985
Polishchuk <s.polishchuk@amiga.agency>
2023-07-04 00:23:02 +0300

    feat: implement 1 - 7 (partial) tasks

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,17 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    mockedAxios.create.mockReturnThis();
+    mockedAxios.get.mockResolvedValueOnce({ data: 1 });
+    await throttledGetDataFromApi('/data');
+
+    expect(mockedAxios.create).toHaveBeenCalled();
   });
 
   test('should perform request to correct provided url', async () => {
